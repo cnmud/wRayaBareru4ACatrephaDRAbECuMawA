a542c23b291657d27b2c166ad6ed825f0d41a9a4
kenny <cnmud@qq.com>
2017-05-21 12:36:50 +0800

    0521

---
+++ 我的家/压缩(dell-pc).cmd
@@ -0,0 +1,1 @@
+"C:\Program Files\WinRAR\rar" a -ep -m5 "D:\我的家\我的家.rar" "C:\Program Files\MoneyWise\MoneyHome\Data\\mhlink.mdb" "C:\Program Files\MoneyWise\MoneyHome\Data\我的家.mh6" "C:\Program Files\MoneyWise\MoneyHome\Data\我的家(20131031).mh6"
---
+++ 我的家/备份.cmd
@@ -0,0 +1,7 @@
+del "I:\My Backup" /Q
+"C:\Program Files\WinRAR\rar" a -m0 -r "I:\My Backup\git.rar" "Z:\git"
+"C:\Program Files\WinRAR\rar" a -m0 -r "I:\My Backup\tfs_prod.rar" "Z:\tfs_prod"
+"C:\Program Files\WinRAR\rar" a -m0 -r "I:\My Backup\tfs2010collection.rar" "Z:\tfs2010collection"
+"C:\Program Files\WinRAR\rar" a -m0 -r "I:\My Backup\vsts21.rar" "Z:\vsts21"
+"C:\Program Files\WinRAR\rar" a -m0 -r "I:\My Backup\vsts23.rar" "Z:\vsts23"
+Pause
